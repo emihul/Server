cb1585f78e9a83f6955bc31fa68d2b31345e1a80
emihul <31851411+emihul@users.noreply.github.com>
2022-05-11 20:43:40 +0200

    Add files via upload

--- a5-pre/app.py
+++ a5-pre/app.py
@@ -24,8 +24,8 @@ if not os.path.exists(PASSWORDFILE):
 
 @app.route('/')
 def home():
-    if status == True:
-        return render_template("loggedin.html", username=log_name)
+    if session.get("username"):
+        return render_template("loggedin.html", username=session.get("username"))
 
     return render_template('home.html')
 
@@ -42,9 +42,14 @@ def register_post():
     password = request.form['password']
     password2 = request.form['matchpassword']
     status = checkpass(password, password2)
+    if(status != 1):
+        return render_template("register.html", error = "password " + status)
+    status = checkname(username)
+    if(status != 1):
+        return render_template("register.html", error = "username " + status)
     if status == 1:
         register_user(username,password)
-        return render_template("login.html")
+        return redirect(url_for("login_get"))
     else:
 
         return render_template("register.html")
@@ -58,27 +63,25 @@ def login_get():
 # Handle login credentials
 @app.route('/login', methods=['POST'])
 def login_post():
-    username = request.form['username']
+    usrname = request.form['username']
     password = request.form['password']
-    status = login(username,password)
+    status = login(usrname,password)
     if status == 1:
-        status = True
-        log_name = username
-        return render_template("loggedin.html", username=log_name)
+        session["username"] = usrname
+        return redirect("/")
     else:
         return render_template('login.html')
 
 
 
 def register_user(name, password):
-    if name in list.keys():
-        return render_template("register.html")
     password = password.encode('utf-8')
     hashedPassword = bcrypt.hashpw(password, bcrypt.gensalt())
     user = {"name":name,"password":hashedPassword.decode('ascii')}
     with open("passwords.json", "w") as fp:
         list.append(user)
         json.dump(list,fp) 
+    
 def login(name,password):
     with open("passwords.json", "r") as fp:
         list = json.load(fp)
@@ -87,36 +90,38 @@ def login(name,password):
                 password = password.encode("utf-8")
                 pass2 = el["password"].encode('ascii')
                 if bcrypt.checkpw(password, pass2):
-                    print("xd")
                     return 1
                 else:
                     return 0
         return 0
 def checkpass(password,pass2):
     if (password != pass2):
-        return 0
+        return "not matching"
     if (len(password)<8):
-        return 0
+        return "too short"
     elif not re.search("[a-z]", password):
-        return 0
+        return "needs lowercase"
     elif not re.search("[A-Z]", password):
-        return 0
+        return "needs uppercase"
     elif not re.search("[0-9]", password):
-        return 0
+        return "needs number"
     elif re.search("\s", password):
-        return 0
+        return "cannot have space"
     else:
         return 1
 def checkname(name):
-    if re.search("\s", name):
-        return 0
+    for el in list:
+            if el["name"] == name:
+                return "name exists"
+    if (len(name)<6):
+        return "too short"
+    elif re.search("\s", name):
+        return "cannot use space"
     else:
         return 1
 if __name__ == '__main__':
 
     # TODO: Add TSL
-    status = False
-    log_name = None
     with open("passwords.json", "r") as fp:
         list = json.load(fp)
     app.run(debug=True)
